d25067591e5e867bc7c754f318b9a960d1c885ec
zanuka <202134+zanuka@users.noreply.github.com>
2018-04-16 16:06:47 +1200

    fixes repo name in vars

--- app/daemon/daemonapi/vars.go
+++ app/daemon/daemonapi/vars.go
@@ -1,6 +1,6 @@
 package daemonapi
 
-import "github.com/Encrypt-S/navpi-go/app/conf"
+import "github.com/Encrypt-S/kauri-api/app/conf"
 
 // NavConfig holds pointer to NavConfig
 // RPCUser     string `json:"rpcUser"`
