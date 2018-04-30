769d2dee717956a97f601a6fc302c378ba83cec7
zanuka <202134+zanuka@users.noreply.github.com>
2018-05-01 11:55:37 +1200

    adds test for getVerboseTx :: updates other tests for new error patterns

--- app/api/wallet/transaction_test.go
+++ app/api/wallet/transaction_test.go
@@ -28,8 +28,13 @@ func mockGetRawTxResponseData() string {
 "id": null}`
 }
 
+// mock data struct for get raw tx verbose response
+func mockGetRawTxVerboseResponseData() string {
+	return `{"result": "123"}`
+}
+
 // mock out the data struct for incoming POST body
-func setupIncomingTestData(t *testing.T) AddressesReq {
+func setupIncomingTestData(t *testing.T) IncomingTransactions {
 	data := `
 	{"transactions": [
     {"currency":  "NAV", "addresses": ["NW7uXr4ZAeJKigMGnKbSLfCBQY59cH1T8G", "NUDke42E3fwLqaBbBFRyVSTETuhWAi7ugk"]},
@@ -40,17 +45,17 @@ func setupIncomingTestData(t *testing.T) AddressesReq {
 
 	r := bytes.NewReader([]byte(data))
 
-	var incomingAddressesReq AddressesReq
+	var incomingAddressesReq IncomingTransactions
 	json.NewDecoder(r).Decode(&incomingAddressesReq)
 
 	// Preflight checks
-	assert.Equal(t, "NAV", incomingAddressesReq.Addresses[0].Currency)
-	assert.Equal(t, "NW7uXr4ZAeJKigMGnKbSLfCBQY59cH1T8G", incomingAddressesReq.Addresses[0].Addresses[0])
-	assert.Equal(t, "NUDke42E3fwLqaBbBFRyVSTETuhWAi7ugk", incomingAddressesReq.Addresses[0].Addresses[1])
+	assert.Equal(t, "NAV", incomingAddressesReq.IncomingTxItems[0].Currency)
+	assert.Equal(t, "NW7uXr4ZAeJKigMGnKbSLfCBQY59cH1T8G", incomingAddressesReq.IncomingTxItems[0].Addresses[0])
+	assert.Equal(t, "NUDke42E3fwLqaBbBFRyVSTETuhWAi7ugk", incomingAddressesReq.IncomingTxItems[0].Addresses[1])
 
-	assert.Equal(t, "BTC", incomingAddressesReq.Addresses[1].Currency)
-	assert.Equal(t, "Bak7ahbZAA", incomingAddressesReq.Addresses[1].Addresses[0])
-	assert.Equal(t, "B91janABsa", incomingAddressesReq.Addresses[1].Addresses[1])
+	assert.Equal(t, "BTC", incomingAddressesReq.IncomingTxItems[1].Currency)
+	assert.Equal(t, "Bak7ahbZAA", incomingAddressesReq.IncomingTxItems[1].Addresses[0])
+	assert.Equal(t, "B91janABsa", incomingAddressesReq.IncomingTxItems[1].Addresses[1])
 
 	return incomingAddressesReq
 }
@@ -65,7 +70,7 @@ func Test_buildResponse(t *testing.T) {
 		httpmock.NewStringResponder(200, mockGetTxIdsResponseData()))
 
 	incomingAddreses := setupIncomingTestData(t)
-	resp := buildResponse(incomingAddreses)
+	resp, _ := buildResponse(incomingAddreses)
 
 	// check that we have only nav currencies
 	for i := range resp.Results {
@@ -82,7 +87,7 @@ func Test_buildResponse(t *testing.T) {
 
 }
 
-
+// test the get transactions function
 func Test_getTransactionsForAddress(t *testing.T) {
 
 	httpmock.Activate()
@@ -93,7 +98,7 @@ func Test_getTransactionsForAddress(t *testing.T) {
 
 	incomingAddresses := setupIncomingTestData(t)
 
-	adds := getTransactionsForAddresses(incomingAddresses.Addresses[0].Addresses)
+	adds, _ := getTransactionsForAddresses(incomingAddresses.IncomingTxItems[0].Addresses)
 
 	// check we have the right amount of addresses
 	assert.Equal(t, 2, len(adds))
@@ -103,8 +108,8 @@ func Test_getTransactionsForAddress(t *testing.T) {
 	assert.Equal(t, "NUDke42E3fwLqaBbBFRyVSTETuhWAi7ugk", adds[1].Address)
 
 	// check for correct txids
-	assert.Equal(t, "11a7071a43a8da2b9ac116865a6cd92c985c3f7cbde63933d253f88dffaa311a", adds[0].Transactions[0].Txid)
-	assert.Equal(t, "c6b6063a0512ed40958bff62a48168b4b30f89cb6bce22b722f8a6d00fcb9d98", adds[0].Transactions[1].Txid)
+	assert.Equal(t, "11a7071a43a8da2b9ac116865a6cd92c985c3f7cbde63933d253f88dffaa311a", adds[0].Transactions[0].TxID)
+	assert.Equal(t, "c6b6063a0512ed40958bff62a48168b4b30f89cb6bce22b722f8a6d00fcb9d98", adds[0].Transactions[1].TxID)
 
 }
 
@@ -117,7 +122,7 @@ func Test_getTxIDsRPC(t *testing.T) {
 	httpmock.RegisterResponder("POST", "http://127.0.0.1:0",
 		httpmock.NewStringResponder(200, mockGetTxIdsResponseData()))
 
-	rpcResp := getTxIdsRPC("NW7uXr4ZAeJKigMGnKbSLfCBQY59cH1T8G")
+	rpcResp, _ := getTxIdsRPC("NW7uXr4ZAeJKigMGnKbSLfCBQY59cH1T8G")
 
 	assert.Equal(t, "11a7071a43a8da2b9ac116865a6cd92c985c3f7cbde63933d253f88dffaa311a", rpcResp.Result[0])
 	assert.Equal(t, "52489abff43212445d432f6042e5b9faf99b3c843a79210629b5383f52694ec5", rpcResp.Result[4])
@@ -133,8 +138,23 @@ func Test_getRawTx(t *testing.T) {
 	httpmock.RegisterResponder("POST", "http://127.0.0.1:0",
 		httpmock.NewStringResponder(200, mockGetRawTxResponseData()))
 
-	rpcResp := getRawTx("11a7071a43a8da2b9ac116865a6cd92c985c3f7cbde63933d253f88dffaa311a")
+	rpcResp, _ := getRawTx("11a7071a43a8da2b9ac116865a6cd92c985c3f7cbde63933d253f88dffaa311a")
 
 	assert.Equal(t, "123", rpcResp.Result)
+
+}
+
+// test that raw (verbose) transaction data is returned from supplied txid
+func Test_getVerboseTx(t *testing.T) {
+
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("POST", "http://127.0.0.1:0",
+		httpmock.NewStringResponder(200, mockGetRawTxVerboseResponseData()))
+
+	rpcResp, _ := getRawTxVerbose("11a7071a43a8da2b9ac116865a6cd92c985c3f7cbde63933d253f88dffaa311a")
+
+	assert.Equal(t, "123", rpcResp)
 
 }
