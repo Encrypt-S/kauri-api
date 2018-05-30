39d0e79a6f0bb3006e5ac43ffffd5ad1f86f9b26
zanuka <202134+zanuka@users.noreply.github.com>
2018-05-30 16:21:54 +1200

    fixes some lint errors

--- app/daemon/daemonapi/vars.go
+++
@@ -1,8 +0,0 @@
-package daemonapi
-
-import "github.com/Encrypt-S/kauri-api/app/conf"
-
-// DaemonConfig holds pointer to DaemonConfig
-// RPCUser     string `json:"rpcUser"`
-// RPCPassword string `json:"rpcPassword"`
-var NavConfig *conf.DaemonConfig
